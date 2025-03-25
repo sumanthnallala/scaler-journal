e1d1042606db7e9615225d4f6650b4d27107d12d
Sumanth Reddy Nallala <nallala.sumanthreddy111@gmail.com>
2025-03-25 21:40:37 +0530

    Good Pair

---
+++ DSA/Arrays/GoodPairs.java
@@ -0,0 +1,40 @@
+package DSA.Arrays;
+
+public class GoodPairs {
+
+  // Given an array A and an integer B.
+  // A pair(i, j) in the array is a good pair if i != j and (A[i] + A[j] == B).
+  // Check if any good pair exist or not.
+
+  // Input format
+  // First argument is an integer array A.
+  // Second argument is an integer B.
+
+  // Output format
+  // Return 1 if good pair exist otherwise return 0.
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    int n = 5;
+    boolean res = goodPair(arr, n);
+    if (res) {
+      System.out.println(1);
+    } else {
+      System.out.println(0);
+    }
+  }
+
+  public static boolean goodPair(int[] arr, int n) {
+    boolean res = false;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] == n) {
+          res = true;
+          break;
+        }
+      }
+    }
+    return res;
+  }
+
+}
