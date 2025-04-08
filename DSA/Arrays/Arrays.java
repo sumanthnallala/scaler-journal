f1c17ee828043e64f51a546bac74cd71387f61ae
Sumanth Reddy Nallala <nallala.sumanthreddy111@gmail.com>
2025-04-08 11:33:43 +0530

    Arrays

--- DSA/Arrays/Arrays.java
+++
@@ -1,5 +0,0 @@
-package DSA.Arrays;
-
-public class Arrays {
-
-}
---
+++ DSA/Arrays/ArraysPractice.java
@@ -0,0 +1,54 @@
+package DSA.Arrays;
+
+import java.util.Arrays;
+
+public class ArraysPractice {
+
+  public static void main(String[] args) {
+//    maxMin();
+    rotateArray();
+  }
+
+  static int maxMin() {
+    int[] A = {1, 2, 3, 4, 5};
+    int max = A[0];
+    int min = A[0];
+    for (int num : A) {
+      if (num > max) {
+        max = num;
+      }
+      if (num < min) {
+        min = num;
+      }
+    }
+    System.out.println(max + min);
+    return max + min;
+  }
+
+  static int[] reverseInRange(int start, int end, int[] A) {
+    while (start < end) {
+      int temp = A[start];
+      A[start] = A[end];
+      A[end] = temp;
+      start++;
+      end--;
+    }
+    System.out.println(Arrays.toString(A));
+    return A;
+  }
+
+  static int[] rotateArray() {
+    int[] A = {1, 2, 3, 4, 5};
+    int n = A.length;
+    int B = 2;
+    if (B > n) {
+      B = B % n;
+    }
+    reverseInRange(0, n-1, A);
+    reverseInRange(0, B-1, A);
+    reverseInRange(B, n-1, A);
+    return A;
+  }
+
+
+}
